e2c8bea91aa69f1ffd00de19819c3683e576221c
Tushar Jadhav <jadhavtusharr1@gmail.com>
2021-09-13 17:14:01 +0530

    Update LoginPage.py

--- LoginPage.py
+++ LoginPage.py
@@ -8,7 +8,7 @@ from twilio.rest import Client
 
 
 # ------------------------ login button function-------------------------
-db1 = pymysql.connect(host='localhost', user='root', password='Death@123', database='hotelui')
+db1 = pymysql.connect(host='localhost', user='root', password='Death'#', database='hotelui')
 
 
 def login():
